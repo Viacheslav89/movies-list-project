7ade18401c498e77fa5cd6c9bf3c28d6775e0bc7
To Do List <2113338@mail.ru>
2024-10-29 19:12:05 +0300

    test hellogit add .

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    hello !!!!!!!!!!!!!!!!!!!!!!!!!11
   </div>
 </template>
